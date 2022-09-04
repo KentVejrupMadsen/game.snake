e57d82ae3d20e774e0a048616a8b6538cc164a93
Kent Madsen <kent.vejrup.madsen@designermadsen.dk>
2022-09-04 10:29:51 +0200

    moving files

--- code/webpack.config.js
+++ code/webpack.config.js
@@ -1,9 +1,11 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack'); //to access built-in plugins
+const path = require('path');
 
-module.exports = {
-    module: {
-        rules: [{ test: /\.txt$/, use: 'raw-loader' }],
+module.exports =
+{
+    entry: 'code.js',
+    output:
+    {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
     },
-    plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
 };
